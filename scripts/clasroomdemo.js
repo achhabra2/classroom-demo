8a6e7599d2f9e822c730cec9c9cb9df2aad8c7a6
Aman Chhabra <aman.chhabra1@gmail.com>
2016-11-05 13:42:42 -0700

    Initial classroom demo script

--- scripts/clasroomdemo.js
+++ scripts/clasroomdemo.js
@@ -1,22 +1,88 @@
-var Conversation = require('hubot-conversation');
+var Conversation = require('hubot-conversation')
+const ciscospark = require(`ciscospark`)
+const art101team = 'Y2lzY29zcGFyazovL3VzL1RFQU0vYzAzMTA3ZTAtYTJjMy0xMWU2LWFhZmQtZDM3MmFlMTY2ZmE5'
+const history101team = 'Y2lzY29zcGFyazovL3VzL1RFQU0vYjMzODkwZDAtYTJjMy0xMWU2LTg3OTktN2JjNjM3YTJhNDZk'
+const math101team = 'Y2lzY29zcGFyazovL3VzL1RFQU0vYjk2YTY5NjAtYTJjMy0xMWU2LTg3OTktN2JjNjM3YTJhNDZk'
+const science101team = 'Y2lzY29zcGFyazovL3VzL1RFQU0vYzc3MjJkOTAtYTJjMy0xMWU2LWFhZmQtZDM3MmFlMTY2ZmE5'
+const english101team = 'Y2lzY29zcGFyazovL3VzL1RFQU0vYWE3YmM3YTAtYTJjMy0xMWU2LWJmOWEtZTU2YmYwYzI4YWI5'
+
 module.exports = function (robot) {
 
-    var switchBoard = new Conversation(robot);
+    var switchBoard = new Conversation(robot)
 
-    robot.respond(/clean the house/, function (msg) {
-        var dialog = switchBoard.startDialog(msg);
+    robot.respond(/Join Class/, function (msg) {
+        console.log("Because I don't know how to use node inspector")
+        ciscospark.people.list({
+          email: msg.message.user.name
+        }).then((people) =>{
+          var person = people.items[0]
+          msg.envelope.user.name = person.displayName
+        })
+        var dialog = switchBoard.startDialog(msg)
 
-        msg.reply('Sure, where should I start? Kitchen or Bathroom');
-        dialog.addChoice(/kitchen/i, function (msg2) {
-            msg2.reply('On it boss!');
-        });
-        dialog.addChoice(/bathroom/i, function (msg2) {
-            msg.reply('Do I really have to?');
-            dialog.addChoice(/yes/, function (msg3) {
-                msg3.reply('Fine, Mom!');
+        msg.send('Sure, I would love to help you with that. Please tell me the number of the class you would like to join:');
+        msg.send('<b>1.</b> Art 101 <br><b>2.</b> English 101 <br><b>3.</b> History 101 <br><b>4.</b> Math 101 <br><b>4.</b> Social Science 101 <br>');
+        dialog.addChoice(/1/i, function (msg2) {
+            ciscospark.teamMemberships.create({
+              personEmail: msg2.message.user.name,
+              teamId: art101team,
+              isModerator: false
             })
-        });
-    });
+            .catch((reason) => {
+              console.error("Did not complete adding to team")
+              console.error(reason)
+            })
+            msg2.send('Ok great I am adding you to the Art 101 Team')
+        })
+        dialog.addChoice(/2/i, function (msg2) {
+          ciscospark.teamMemberships.create({
+            personEmail: msg2.message.user.name,
+            teamId: english101team,
+            isModerator: false
+          })
+          .catch((reason) => {
+            console.error("Did not complete adding to team")
+            console.error(reason)
+          })
+            msg2.send('Ok great I am adding you to the English 101 Team')
+        })
+        dialog.addChoice(/3/i, function (msg2) {
+          ciscospark.teamMemberships.create({
+            personEmail: msg2.message.user.name,
+            teamId: history101team,
+            isModerator: false
+          })
+          .catch((reason) => {
+            console.error("Did not complete adding to team")
+            console.error(reason)
+          })
+            msg2.send('Ok great I am adding you to the History 101 Team')
+        })
+        dialog.addChoice(/4/i, function (msg2) {
+          ciscospark.teamMemberships.create({
+            personEmail: msg2.message.user.name,
+            teamId: math101team,
+            isModerator: false
+          })
+          .catch((reason) => {
+            console.error("Did not complete adding to team")
+            console.error(reason)
+          })
+            msg2.send('Ok great I am adding you to the Math 101 Team')
+        })
+        dialog.addChoice(/5/i, function (msg2) {
+          ciscospark.teamMemberships.create({
+            personEmail: msg2.message.user.name,
+            teamId: science101team,
+            isModerator: false
+          })
+          .catch((reason) => {
+            console.error("Did not complete adding to team")
+            console.error(reason)
+          })
+            msg2.send('Ok great I am adding you to the Social Science 101 Team')
+        })
+    })
 
     robot.respond(/jump/, function (msg) {
         var dialog = switchBoard.startDialog(msg);
